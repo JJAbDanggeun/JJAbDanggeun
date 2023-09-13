66a57d7c1df0837d693c78add17e23711a63ca5f
HyoJeong-Park <parkhj3003@gmail.com>
2023-09-13 20:20:11 +0900

    chore: 폰트 파일 추가 및 폰트 글로벌 설정

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import Providers from "./components/providers/Providers.tsx";
+import { GlobalStyle } from "./global.ts";
 //import { worker } from "./mocks/worker.ts";
 
 // if (process.env.NODE_ENV === "development") {
@@ -12,6 +13,7 @@ import Providers from "./components/providers/Providers.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<Providers>
+			<GlobalStyle />
 			<App />
 		</Providers>
 	</React.StrictMode>,
